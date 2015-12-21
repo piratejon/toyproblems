a76b3afc26948a577b765e79475df12123e01659
Jonathan Wesley Stone <js@piratejon.com>
2015-12-21 00:18:38 -0600

    well that gave an answer by luck and Ctrl+C or something!

---
+++ 15/lol.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+import fileinput
+
+import random
+
+def parse_line(line):
+    w = line.split()
+    return {
+        w[0][:-1]: {
+            w[i]: int(w[i + 1].split(',')[0]) for i in range(1, len(w), 2)
+        }
+    }
+
+def score_up(costs, qty):
+    weight = 1
+    for factor in costs.keys():
+        if factor != 'calories':
+            weight *= max(0, sum([qty[k] * costs[factor][k] for k in qty]))
+    return weight
+
+def goofy_ass_walk(costs, ingreds, a_hundred):
+    """Try to increase things that improve the objective function?"""
+    # make an initial guess that adds up to 100
+
+    initial_score = 0
+    while initial_score == 0:
+        guesslist = [random.random() for _ in ingreds]
+        scale = sum(guesslist)
+        guesslist = [int(a_hundred  * _ / scale) for _ in guesslist]
+        guesslist[-1] = a_hundred - sum(guesslist[:-1])
+        guess = {
+#        _: int(a_hundred / len(ingreds)) for _ in ingreds
+            k: v for k, v in zip(ingreds, guesslist)
+        }
+        initial_score = score_up(costs, guess)
+
+    print('initial guess', guess, initial_score)
+
+    while True:
+        current_best = score_up(costs, guess)
+        for i in ingreds:
+            for j in ingreds:
+                if i != j and guess[i] < 100 and guess[j] > 0:
+                    guess[i] += 1
+                    guess[j] -= 1
+                    trial_score = score_up(costs, guess)
+                    if trial_score > current_best:
+                        current_best = trial_score
+                        print('found new best', guess, current_best)
+                    else:
+                        guess[i] -= 1
+                        guess[j] += 1
+        # don't loop forever if we did not make an improvement
+        if initial_score == current_best:
+            break
+
+    return guess
+
+def main():
+    ingred = {}
+    for line in fileinput.input():
+        ingred.update(parse_line(line))
+
+    costs = {}
+    for k, v in ingred.items():
+        for c, w in v.items():
+            if c not in costs:
+                costs[c] = {}
+            costs[c][k] = w
+
+    result = goofy_ass_walk(costs, ingred.keys(), 100)
+    print(result, score_up(costs, result)) 
+
+if __name__ == '__main__':
+    main()
+
