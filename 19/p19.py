34d1bfc8a59481090dabd05203d95733a352894c
Jonathan Wesley Stone <js@piratejon.com>
2015-12-24 11:34:41 -0600

    do a capability

--- 19/p19.py
+++ 19/p19.py
@@ -6,29 +6,41 @@ class P19:
 
     def __init__(self):
         self.replacements = {}
+        self.invreplacements = {}
         self.molecules = set()
 
     def add_replacement(self, left, right):
         if left not in self.replacements:
             self.replacements[left] = set()
+        if right not in self.invreplacements:
+            self.invreplacements[right] = set()
         self.replacements[left].add(right)
+        self.invreplacements[right].add(left)
 
     def do_it_to_it(self, what):
-        orig_what = what
         self.molecules = set()
         for k in self.replacements:
             i = what.find(k)
             if i == -1:
                 pass
-#self.molecules.add(what)
+                #print('did not find', k, 'in', what)
             else:
                 while i > -1:
                     for v in self.replacements[k]:
-                        print('found', k, v, 'in', what, 'at', i)
-                        adding = what[:i] + v + what[i + len(v) - 2:]
-                        print(what[:i], v, what[i + len(v) - 2:])
+#print('found', k, v, 'in', what, 'at', i)
+                        adding = what[:i] + v + what[i + len(k):]
                         self.molecules.add(adding)
                     i = what.find(k, i + 1)
+
+    def pieces(self, string, pos):
+        self.piecebag = {}
+        for i in range(len(string) - pos):
+            if string[pos:pos+i] in self.invreplacements:
+                if i not in self.piecebag:
+                    self.piecebag[i] = set()
+                self.piecebag[i] |= self.invreplacements[string[pos:pos+i]]
+
+        return self.piecebag
 
 def main():
     p = P19()
@@ -46,7 +58,7 @@ def main():
     p.do_it_to_it(thing)
 
     print('molecules:', len(p.molecules))
-#    print(p.molecules)
+    print(p.molecules)
 
 if __name__ == '__main__':
     main()
--- 19/test_thing.py
+++ 19/test_thing.py
@@ -16,6 +16,35 @@ class TestP19(unittest.TestCase):
         p.do_it_to_it('HOHOHO')
         self.assertEqual(len(p.molecules), 7)
 
+    def test_rednose(self):
+        p = p19.P19()
+        p.add_replacement('e', 'H')
+        p.add_replacement('e', 'O')
+        p.add_replacement('H', 'HO')
+        p.add_replacement('H', 'OH')
+        p.add_replacement('O', 'HH')
+
+#        self.assertEqual(p.pieces('HOH', 0), {'e': {'H'}, 'H': {'HO'}})
+#        self.assertEqual(p.pieces('HOH', 1), {'e': {'O'}, 'H': {'OH'}})
+#        self.assertEqual(p.pieces('HOH', 2), {'e': {'H'}})
+
+    def test_ruffian(self):
+        p = p19.P19()
+        instr = ''
+        mode = 'rules'
+        with open('input', 'r') as fin:
+            for line in fin.readlines():
+                if mode == 'rules' and len(line.strip()) > 0:
+                    words = line.split()
+                    p.add_replacement(words[0], words[2])
+                else:
+                    mode = 'input'
+                    instr = line.strip()
+
+        self.assertEqual(len(instr), 468)
+
+        self.assertEqual(p.pieces(instr, 3), {4: {'Si'}})
+
 if __name__ == '__main__':
     unittest.main()
 
