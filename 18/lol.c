dc98b68074e82ba07e7d6083063545f74920a7ab
Jonathan Wesley Stone <js@piratejon.com>
2015-12-24 02:37:43 -0600

    woop woop

--- 18/lol.c
+++ 18/lol.c
@@ -21,7 +21,8 @@ void load_data_file(const char * filename, char (*lights)[100][100]) {
   fin = fopen(filename, "r");
 
   for (i = 0; i < 100; i += 1) {
-    fgets((*lights)[i], 100, fin);
+    // + 2, for the carriage return and terminal
+    fgets((*lights)[i], 100 + 2, fin);
   }
 
   fclose(fin);
@@ -32,15 +33,31 @@ int count_on_neighbors (char (*grid)[100][100], int I, int J) {
 
   count = 0;
 
+  // printf("counting neighbors for (%d,%d):\n", I, J);
+
   for (i = I - 1; i <= I + 1; i += 1) {
     for (j = J - 1; j <= J + 1; j += 1) {
-      if (i >= 0 && j >= 0 && i < 100 && j < 100 && (*grid)[i][j] == '#') {
+      if ((i != I || j != J) && i >= 0 && j >= 0 && i < 100 && j < 100 && (*grid)[i][j] == '#') {
+        // printf("  (%d,%d) is on\n", i, j);
         count += 1;
       }
     }
   }
 
+  // printf("  (%d,%d) has %d on neighbors\n", I, J, count);
+
   return count;
+}
+
+void print_board(char (*g)[100][100]) {
+  int i, j;
+
+  for (i = 0; i < 100; i += 1) {
+    for (j = 0; j < 100; j += 1) {
+      // printf("%c", (*g)[i][j]);
+    }
+    // printf("\n");
+  }
 }
 
 void do_steps(char (*current)[100][100], char (*new)[100][100]) {
@@ -49,15 +66,27 @@ void do_steps(char (*current)[100][100], char (*new)[100][100]) {
   for (i = 0; i < 100; i += 1) {
     for (j = 0; j < 100; j += 1) {
       count = count_on_neighbors(current, i, j);
+      // printf("(%d,%d)", i, j);
       if ((*current)[i][j] == '#') {
-        if (count != 2 && count != 3) {
+        // printf(" is on");
+        if (count == 2 || count == 3) {
+          // printf(" and stays on");
+          (*new)[i][j] = '#';
+        } else {
+          // printf(" and turns off");
           (*new)[i][j] = '.';
         }
       } else {
+        // printf(" is off");
         if (count == 3) {
+          // printf(" and turns on");
           (*new)[i][j] = '#';
+        } else {
+          // printf(" and stays off");
+          (*new)[i][j] = '.';
         }
       }
+      // printf(" (%d)\n", count);
     }
   }
 }
@@ -68,10 +97,23 @@ void do_a_bunch_of_steps(int i) {
   lights_b = &LIGHTS_B;
   while (i -- > 0) {
     do_steps(lights_a, lights_b);
-
+    // printf("step:\n");
+    print_board(lights_b);
+    // printf("\n");
     lights_tmp = lights_a;
     lights_a = lights_b;
     lights_b = lights_tmp;
+  }
+  printf("%d\n", count_on(lights_a));
+  printf("%d\n", count_on(lights_b));
+}
+
+void init_board(char (*g)[100][100]) {
+  int i, j;
+  for (i = 0; i < 100; i += 1) {
+    for (j = 0; j < 100; j += 1) {
+      (*g)[i][j] = '.';
+    }
   }
 }
 
@@ -79,9 +121,13 @@ int main (int arfc, char ** arfv) {
 
   load_data_file(arfv[1], &LIGHTS_A);
 
-  do_a_bunch_of_steps(100);
+  init_board(&LIGHTS_B);
 
-  printf("%d\n", count_on(&LIGHTS_A));
+  // printf("initial board\n");
+  print_board(&LIGHTS_A);
+  // printf("\n");
+
+  do_a_bunch_of_steps(100);
 
   return 0;
 }
