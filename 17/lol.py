eed6b26f6ff8cdbb2129cca5959035639dd0d003
Jonathan Wesley Stone <js@piratejon.com>
2015-12-24 01:17:24 -0600

    lol

--- 17/lol.py
+++ 17/lol.py
@@ -3,6 +3,18 @@
 import fileinput
 
 WINNERS=0
+COUNTWISE={}
+
+def part2(c, goal, soln = []):
+    global COUNTWISE
+    for i, v in enumerate(c):
+        soln.append(v)
+        part2(c[i + 1:], goal, soln)
+        if sum(soln) == goal:
+            if len(soln) not in COUNTWISE:
+                  COUNTWISE[len(soln)] = 0
+            COUNTWISE[len(soln)] += 1
+        soln.pop()
 
 def container_it_up_listwise(c, goal, soln = []):
     global WINNERS
@@ -20,9 +32,11 @@ def main():
               containers.append(int(line.strip()))
 
     print(containers)
-    container_it_up_listwise(sorted(containers), 150)
-
+    container_it_up_listwise(sorted(containers), 25)
     print(WINNERS)
+
+    part2(sorted(containers), 25)
+    print(COUNTWISE)
 
 if __name__ == '__main__':
     main()
