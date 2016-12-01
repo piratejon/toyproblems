4b290b762f3154954132ca0936320a850325d1ea
Jonathan Wesley Stone <js@piratejon.com>
2016-11-30 23:57:12 -0600

    solved the second part whoohoo

--- adventofcode/2016/01/01.sql
+++ adventofcode/2016/01/01.sql
@@ -20,6 +20,7 @@ select
   , sum(case dir when 'N' then steps when 'S' then -steps else 0 end) over (order by n) y
   , abs(sum(case dir when 'E' then steps when 'W' then -steps else 0 end) over (order by n))
   + abs(sum(case dir when 'N' then steps when 'S' then -steps else 0 end) over (order by n)) total
+into tmp_2016_01b
 from (
   select
     a
@@ -62,5 +63,63 @@ from (
     ) z
   ) w
 ) v
+;
+
+-- 257 is too high
+with recursive fill_in_steps as (
+  select
+    a
+    , turn
+    , steps - 1 steps
+    , bearing
+    , dir
+    , n
+    , case dir when 'E' then 1 when 'W' then -1 else 0 end x
+    , case dir when 'N' then 1 when 'S' then -1 else 0 end y
+  from tmp_2016_01b
+  union all
+  select
+    s.a
+    , s.turn
+    , s.steps - 1
+    , s.bearing
+    , s.dir
+    , s.n
+    , case s.dir when 'E' then 1 when 'W' then -1 else 0 end
+    , case s.dir when 'N' then 1 when 'S' then -1 else 0 end
+  from fill_in_steps s
+  inner join tmp_2016_01b e
+    on s.n + 1 = e.n
+    and s.steps > 0
+)
+select
+  *
+from (
+  select
+    a
+    , turn
+    , steps
+    , bearing
+    , dir
+    , n
+    , x
+    , y
+    , abs(x) + abs(y) total
+    , row_number() over (partition by x, y order by n, steps desc) rn
+  from (
+    select
+      a
+      , turn
+      , steps
+      , bearing
+      , dir
+      , n
+      , sum(x) over (order by n, steps desc) x
+      , sum(y) over (order by n, steps desc) y
+    from fill_in_steps
+  ) x
+) y
+where rn > 1
+order by n, steps desc
 ;
 
