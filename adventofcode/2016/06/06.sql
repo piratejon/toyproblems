660ced730c534ec65be7c11387ec36226c57b694
Jonathan Wesley Stone <js@piratejon.com>
2016-12-05 23:07:12 -0600

    6.1

---
+++ adventofcode/2016/06/06.sql
@@ -0,0 +1,48 @@
+
+drop table if exists tmp_2016_06
+;
+
+create table tmp_2016_06 (input text)
+;
+
+\copy tmp_2016_06 from './input'
+;
+
+drop table if exists tmp_2016_06b
+;
+
+with tmp as (
+  select
+    unnest(s) k
+    , generate_subscripts(s, 1) pos
+    , line
+  from (
+    select
+      string_to_array(input, NULL) s
+      , row_number() over () as line
+    from tmp_2016_06
+  ) y
+)
+
+select
+  *
+from (
+  select
+    k
+    , pos
+    , c
+    , row_number() over (partition by pos order by c desc) r
+  from (
+    select
+      k
+      , pos
+      , count(*) c
+    from tmp
+    group by k, pos
+    order by pos, count(*) desc
+  ) z
+) y
+where r = 1
+order by pos
+;
+
