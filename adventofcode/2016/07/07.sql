da9232e80fa8cdbc5922f08f9315b8adf8ae8e26
Jonathan Wesley Stone <js@piratejon.com>
2016-12-06 23:34:42 -0600

    yay 343rd place :(

--- adventofcode/2016/07/07.sql
+++ adventofcode/2016/07/07.sql
@@ -5,7 +5,7 @@ drop table if exists tmp_2016_07
 create table tmp_2016_07 (input text)
 ;
 
-\copy tmp_2016_07 from './test'
+\copy tmp_2016_07 from './input'
 ;
 
 drop table if exists tmp_2016_07b
@@ -14,34 +14,44 @@ drop table if exists tmp_2016_07b
 -- 65 is too low
 select
   line
+  , input
 from (
   select
     line
-    , case when a[1] = a[2] then 'n' else 'y' end
-    , case when b[1] = b[2] then 'n' else 'y' end
+    , input
+    , sum(case when h = 1 and abba = 1 then 1 else 0 end) h
+    , sum(abba) abba
   from (
     select
-      input
-      , regexp_matches(input, '[^[]?([^\]])([^\]])\2\1', 'g') a
-      , regexp_matches(input, '[^\]]?([^\]])([^\]])\2\1', 'g') b
-      , row_number() over () as line
-    from tmp_2016_07
-  ) z
-  where a[1] <> a[2] or b[1] <> b[2]
-) x
-except
-select
-  line
-from (
-  select
-    line
-    , case when c[1] = c[2] then 'n' else 'y' end
-  from (
-    select
-      input
-      , regexp_matches(input, '\[.*([^\]])([^\]])\2\1.*\]', 'g') c
-      , row_number() over () as line
-    from tmp_2016_07
-  ) z
-  where c[1] <> c[2]
-) x
+      line
+      , input
+      , c
+      , i
+      , sum(case c when '[' then 1 when ']' then -1 else 0 end) over (partition by line order by i) - case when c = '[' then 1 else 0 end h
+      , case
+        when lag(c, 3) over (partition by line order by i) = c
+          and lag(c, 2) over (partition by line order by i) = lag(c, 1) over (partition by line order by i)
+          and c <> lag(c, 1) over (partition by line order by i)
+        then 1
+      else 0
+      end abba
+    from (
+      select
+        line
+        , input
+        , unnest(a) c
+        , generate_subscripts(a, 1) i
+      from (
+        select
+          row_number() over () as line
+          , input
+          , string_to_array(input, NULL) a
+        from tmp_2016_07
+      ) z
+    ) y
+  ) x
+  group by 1,2
+) w
+where h = 0 and abba > 0
+;
+
