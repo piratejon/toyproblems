ff7ef9c7392c931afa775a49c662012a482dab9a
Jonathan Wesley Stone <js@piratejon.com>
2016-12-09 17:58:36 -0600

    add test inputs and output for 8

--- adventofcode/2016/08/08.sql
+++ adventofcode/2016/08/08.sql
@@ -2,6 +2,9 @@
 drop table if exists tmp_2016_08
 ;
 
+drop table if exists tmp_2016_08_final_state
+;
+
 create table tmp_2016_08 (input text)
 ;
 
@@ -112,13 +115,84 @@ with recursive input_stream as (
   inner join work_input_stream w
     on i.line = w.line + 1
 )
-select sum(v) from (
+, final_state as (
   select
     *
-    , row_number() over (partition by x, y order by line desc) l
-  from work_input_stream
-  -- order by line, x, y
+  from (
+    select
+      *
+      , row_number() over (partition by x, y order by line desc) l
+    from work_input_stream
+    -- order by line, x, y
+  )
+  x where l = 1
 )
-x where l = 1
+select
+  *
+into tmp_2016_08_final_state
+from final_state
+;
+
+select
+  sum(v) part1
+from tmp_2016_08_final_state
+;
+
+select
+  *
+from crosstab('select y, x, case v when 1 then ''X'' end v from tmp_2016_08_final_state order by y, x')
+as ct(
+  y int
+  , "0" text
+  , "1" text
+  , "2" text
+  , "3" text
+  , "4" text
+  , "5" text
+  , "6" text
+  , "7" text
+  , "8" text
+  , "9" text
+  , "10" text
+  , "11" text
+  , "12" text
+  , "13" text
+  , "14" text
+  , "15" text
+  , "16" text
+  , "17" text
+  , "18" text
+  , "19" text
+  , "20" text
+  , "21" text
+  , "22" text
+  , "23" text
+  , "24" text
+  , "25" text
+  , "26" text
+  , "27" text
+  , "28" text
+  , "29" text
+  , "30" text
+  , "31" text
+  , "32" text
+  , "33" text
+  , "34" text
+  , "35" text
+  , "36" text
+  , "37" text
+  , "38" text
+  , "39" text
+  , "40" text
+  , "41" text
+  , "42" text
+  , "43" text
+  , "44" text
+  , "45" text
+  , "46" text
+  , "47" text
+  , "48" text
+  , "49" text
+)
 ;
 
