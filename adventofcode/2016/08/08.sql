2b82fbbdf7f771b2858545717866889502c1e6cb
Jonathan Wesley Stone <js@piratejon.com>
2016-12-08 23:06:20 -0600

    this could really be it this time

--- adventofcode/2016/08/08.sql
+++ adventofcode/2016/08/08.sql
@@ -5,7 +5,7 @@ drop table if exists tmp_2016_08
 create table tmp_2016_08 (input text)
 ;
 
-\copy tmp_2016_08 from './input'
+\copy tmp_2016_08 from './test'
 ;
 
 -- 19 is too low
@@ -27,67 +27,95 @@ with recursive input_stream as (
       row_number() over () as line
       , input
       , regexp_split_to_array(input, E' \|=') a
-      , 50 w
-      , 6 h
+      , 7 w
+      , 3 h
     from tmp_2016_08
+    -- limit 4
   ) z
 )
-, update_state as (
+, prepare_grid as (
   select
-    line
-    , input
-    , w
-    , h
-    , cmd
-    , a
-    -- rotation as the first instruction shouldn't do anything
-    , null::int ox
-    , null::int oy
-    , case when cmd = 'rect' then generate_series(0, a[1]::int - 1) end x
-    , case when cmd = 'rect' then generate_series(0, a[2]::int - 1) end y
-    , row_number() over (order by line) r
-  from input_stream
-  where line = 1
+    generate_series(0, w - 1) x
+    , generate_series(0, h - 1) y
+  from (
+    select distinct w, h from input_stream
+  ) x
+)
+, work_input_stream as (
+  select
+    i.line
+    , i.input
+    , i.w
+    , i.h
+    , i.cmd
+    , i.a
+    , p.x
+    , p.y
+    , case when i.cmd = 'rect' and p.x < i.a[1]::int and p.y < i.a[2]::int then 1 else 0 end v
+  from input_stream i
+  cross join prepare_grid p
+  where i.line = 1
 
   union all
 
-  select -- distinct
-    n.line
-    , n.input
-    , n.w
-    , n.h
-    , n.cmd
-    , n.a
-    , u.x ox
-    , u.y oy
+  select
+    i.line
+    , i.input
+    , i.w
+    , i.h
+    , i.cmd
+    , i.a
+    , w.x
+    , w.y
     , case
-    when n.cmd = 'rect' and n.line = u.line + 2 then u.x
-    when n.cmd = 'rect' then generate_series(0, n.a[1]::int - 1)
-    when n.cmd = 'rotate' and n.a[1] = 'row' and n.a[2] = 'y' and u.y = n.a[3]::int then
-      -- rotating a row increases x values
-      (u.x + n.a[5]::int) % n.w
-    else u.x
-    end x
-    , case
-    when n.cmd = 'rect' and n.line = u.line + 2 then u.y
-    when n.cmd = 'rect' then generate_series(0, n.a[2]::int - 1)
-    when n.cmd = 'rotate' and n.a[1] = 'column' and n.a[2] = 'x' and u.x = n.a[3]::int then
-      -- rotating a column moves y values down
-      (u.y + n.a[5]::int) % n.h
-    else u.y
-    end y
-    , row_number() over (order by n.line) r
-  from input_stream n
-  inner join update_state u
-    -- normal increment
-    on n.line = u.line + 1
-    -- generate_series overwrites the values so we have to repeat them
-    or n.cmd = 'rect' and n.line = u.line + 2
+    when i.cmd = 'rect'
+      and w.x < i.a[1]::int
+      and w.y < i.a[2]::int
+      then 1
+
+    when i.cmd = 'rotate'
+      and i.a[1] = 'column'
+      and i.a[2] = 'x'
+      and i.a[3]::int = w.x
+      and w.y >= i.a[5]::int
+      then lag(w.v, i.a[5]::int) over (partition by i.line, w.x order by w.y)
+
+    when i.cmd = 'rotate'
+      and i.a[1] = 'column'
+      and i.a[2] = 'x'
+      and i.a[3]::int = w.x
+      and w.y < i.a[5]::int
+      then lead(w.v, i.h - i.a[5]::int) over (partition by i.line, w.x order by w.y)
+
+    when i.cmd = 'rotate'
+      and i.a[1] = 'row'
+      and i.a[2] = 'y'
+      and i.a[3]::int = w.y
+      and w.x >= i.a[5]::int
+      then lag(w.v, i.a[5]::int) over (partition by i.line, w.y order by w.x)
+
+    when i.cmd = 'rotate'
+      and i.a[1] = 'row'
+      and i.a[2] = 'y'
+      and i.a[3]::int = w.y
+      and w.x < i.a[5]::int
+      then lead(w.v, i.w - i.a[5]::int) over (partition by i.line, w.y order by w.x)
+
+    else v
+    end v
+  from input_stream i
+  inner join work_input_stream w
+    on i.line = w.line + 1
 )
-select distinct
-line , input , cmd , a , x , y
-from update_state
--- group by line order by line desc
-order by line, x, y -- , ox, oy, r
+select
+  line
+  , input
+  , cmd
+  , a
+  , x
+  , y
+  , v
+from work_input_stream
+order by line, x, y
 ;
 
