e5a0f547ff88bb86a984aeaa29f553a0fc4da65f
Jonathan Wesley Stone <js@piratejon.com>
2016-12-08 21:29:25 -0600

    this is taking an awful long time i hope it is not wrong

--- adventofcode/2016/08/08.sql
+++ adventofcode/2016/08/08.sql
@@ -13,6 +13,8 @@ with recursive input_stream as (
   select
     line
     , input
+    , w
+    , h
     , a[1] cmd
     , case
       when a[1] = 'rect'
@@ -25,64 +27,67 @@ with recursive input_stream as (
       row_number() over () as line
       , input
       , regexp_split_to_array(input, E' \|=') a
-      , 7 w
-      , 3 h
+      , 50 w
+      , 6 h
     from tmp_2016_08
   ) z
 )
 , update_state as (
   select
     line
-    -- , input
+    , input
+    , w
+    , h
     , cmd
     , a
     -- rotation as the first instruction shouldn't do anything
     , null::int ox
     , null::int oy
-    , null::int ov
     , case when cmd = 'rect' then generate_series(0, a[1]::int - 1) end x
     , case when cmd = 'rect' then generate_series(0, a[2]::int - 1) end y
-    , null::int v -- case when cmd = 'rect' then 1 end v
+    , row_number() over (order by line) r
   from input_stream
   where line = 1
 
   union all
 
-  select distinct
+  select -- distinct
     n.line
-    -- , n.input
+    , n.input
+    , n.w
+    , n.h
     , n.cmd
     , n.a
-    , u.x
-    , u.y
-    , u.v
+    , u.x ox
+    , u.y oy
     , case
+    when n.cmd = 'rect' and n.line = u.line + 2 then u.x
     when n.cmd = 'rect' then generate_series(0, n.a[1]::int - 1)
     when n.cmd = 'rotate' and n.a[1] = 'row' and n.a[2] = 'y' and u.y = n.a[3]::int then
       -- rotating a row increases x values
-      (u.x + n.a[5]::int) % 50
+      (u.x + n.a[5]::int) % n.w
     else u.x
     end x
     , case
+    when n.cmd = 'rect' and n.line = u.line + 2 then u.y
     when n.cmd = 'rect' then generate_series(0, n.a[2]::int - 1)
     when n.cmd = 'rotate' and n.a[1] = 'column' and n.a[2] = 'x' and u.x = n.a[3]::int then
       -- rotating a column moves y values down
-      (u.y + n.a[5]::int) % 6
+      (u.y + n.a[5]::int) % n.h
     else u.y
     end y
-    , null::int v -- case when n.cmd = 'rect' then 1 end v
+    , row_number() over (order by n.line) r
   from input_stream n
   inner join update_state u
+    -- normal increment
     on n.line = u.line + 1
+    -- generate_series overwrites the values so we have to repeat them
+    or n.cmd = 'rect' and n.line = u.line + 2
 )
-select
-  line
-  , count(*)
+select distinct
+line , input , cmd , a , x , y
 from update_state
-group by
-  line
-order by
-  line desc
--- order by line, x, y
+-- group by line order by line desc
+order by line, x, y -- , ox, oy, r
 ;
 
