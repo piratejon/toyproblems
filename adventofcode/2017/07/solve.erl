4734a57e19a9358200116f4e0435c1a4d234b256
Jonathan Wesley Stone <js@piratejon.com>
2017-12-08 18:27:54 -0600

    hoppin' boxes again

--- adventofcode/2017/07/solve.erl
+++ adventofcode/2017/07/solve.erl
@@ -3,20 +3,28 @@
 
 parse_line(Line) ->
   % turn the line into a name+weight, and optional children
-  Parts = re:split(Line, "[ ,\\(\\)\\->]", [{return, list}]),
-  [Name, _, Weight | Rest] = Parts,
-  Children = lists:filter(fun(X) -> X /= "" end, Rest),
-  %io:fwrite("Name: ~s, Weight: ~s, Child Count: ~w, Children: ~s~n", [Name, Weight, length(Children), Children]),
-  {Name, Weight, Children}.
+  Len = string:length(Line),
+  if Len > 0 ->
+    Parts = re:split(Line, "[ ,\\(\\)\\->]", [{return, list}]),
+    [Name, _, WeightStr | Rest] = Parts,
+    Children = lists:filter(fun(X) -> X /= "" end, Rest),
+    io:fwrite("Name: ~s, Weight: ~s, Child Count: ~w, Children: ~s~n", [Name, WeightStr, length(Children), Children]),
+    {ok, Name, string:to_integer(WeightStr), Children};
+    true -> {empty}
+  end.
+
+build_tree(Items) ->
+  lists:foldl(fun({ok, Item, Weight, Children}, Dict) -> dict:store(Item, Weight, Dict) end, dict:new(), Items).
 
 read_lines(Filename) ->
   {ok, Data} = file:read_file(Filename),
   Lines = binary:split(Data, [<<"\n">>], [global]),
-  lists:map(fun parse_line/1, Lines).
+  io:fwrite("List: ~w~n", [Lines]),
+  dict:to_list(build_tree(lists:map(fun parse_line/1, lists:filter(fun(X) -> string:length(X) > 0 end, Lines)))).
 
 read_the_file(Filename) ->
   Lines = read_lines(Filename),
-  io:fwrite("~s~n", [Lines]).
+  io:fwrite("List: ~w~n", [Lines]).
 
 part1() ->
   read_the_file('test.input').
