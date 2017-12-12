57469112a47f2187d3762bc54a2737ebbafe4b67
Jonathan Wesley Stone <js@piratejon.com>
2017-12-11 21:53:05 -0600

    uggh finally day 10 part 1

--- adventofcode/2017/10/solve.py
+++ adventofcode/2017/10/solve.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env
 
+'''solve aoc 2017 day 10'''
+
 import sys
-
-ropelen, lengths = 5, [3, 4, 1, 5]
-#ropelen, lengths = 256, [120,93,0,90,5,80,129,74,1,165,204,255,254,2,50,113]
-
-llist = [_ for _ in range(ropelen)]
 
 class Knot:
     '''implement a hashable knot'''
@@ -18,61 +15,41 @@ class Knot:
         self.skip = 0
 
     def __getitem__(self, idx):
-        # index modulo len(list)
-        #print(idx)
+        '''index modulo len(list)'''
+        print(idx)
         if type(idx).__name__ == 'slice':
             start, stop = idx.start, idx.stop
-            start %= self.len
-            stop %= self.len
-            print(idx, start, stop)
-            if start > stop: # wrapping
-                #print('wrapping', idx)
-                left = self.list[:stop]
-                right = self.list[start:]
-                middle = self.list[stop:start]
-                #print(left, middle, right)
-                return left + middle + right
-            else:
-                #print('nonwrapping', idx)
-                return self.list[idx]
-        elif type(idx) == 'int':
+            return [self.list[_ % self.len] for _ in range(start, stop)]
+        elif isinstance(idx, int):
             return self.list[idx % self.len]
 
     def __setitem__(self, idx, val):
-        if type(val).__name__ == 'slice':
+        '''allow setting with idx modulo len(list)'''
+        print('set', idx, val)
+        if type(idx).__name__ == 'slice':
             start, stop = idx.start, idx.stop
-            start %= self.len
-            stop %= self.len
-            if start > stop: # wrapping
-                left = self.list[:stop]
-                right = self.list[start:]
-                middle = self.list[stop:start]
-                for i in range(len(val)):
-                    if i < len(right):
-                        right[i] = val[i]
-                    else:
-                        left[self.len - stop + i] = val[i]
-                return left + middle + right
-            else:
-                for i in range(start, stop):
-                    self.list[i % self.len] = val[i - start]
-        elif type(val).__name__ == 'int':
+            for _ in range(start, stop):
+                self[_] = val[_ - start]
+        elif type(idx).__name__ == 'int':
             self.list[idx % self.len] = val
 
     def do_it(self, length):
         '''hash iter given this length'''
         if length < 0 or length > self.len:
             raise ValueError(length)
-        print(self.list, self.curpos, self.skip, length)
+        print('List:', self.list, 'curpos:', self.curpos, 'skiplen:', self.skip, 'Length:', length)
         self.reverse(length)
 
+        self.curpos += length + self.skip
         self.skip += 1
-        self.curpos += length
 
     def reverse(self, length):
         '''reverse length items starting at curpos+skip'''
-        revme = self[self.curpos + self.skip:self.curpos + self.skip + length]
-        print('reverse', length, len(revme), revme)
+        print('reverse', length, self.curpos, self.curpos + length)
+        revme = self[self.curpos:self.curpos + length]
+        reved = [_ for _ in reversed(revme)]
+        print('reverse', length, len(revme), revme, reved)
+        self[self.curpos:self.curpos + length] = reved
 
 def main(filename):
     '''driver'''
@@ -86,6 +63,9 @@ def main(filename):
 
     for _ in llist:
         knot.do_it(_)
+        print()
+    print(knot.list)
+    print(knot.list[0] * knot.list[1])
 
 if __name__ == '__main__':
     main(sys.argv[1] if len(sys.argv) == 2 else None)
