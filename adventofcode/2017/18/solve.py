0694ded2c1d9e427c97bdf9a84ad7ed60163dd5a
Jonathan Wesley Stone <js@piratejon.com>
2017-12-19 05:48:31 -0600

    refactor in preparation for part 2

--- adventofcode/2017/18/solve.py
+++ adventofcode/2017/18/solve.py
@@ -2,46 +2,56 @@
 
 import sys
 
-with open('test.input' if len(sys.argv) != 2 else sys.argv[1], 'r') as fin:
-    reg = {chr(ord('a') + i): 0 for i in range(26)}
-    insns = [_.split() for _ in fin]
-    sounds = []
+class Program:
+    '''encapsulate the program so we can run two at once'''
 
-    pc = 0
-    while pc >= 0 and pc < len(insns):
-        i = insns[pc]
-        print(pc, i)
-        if i[0] == 'set':
-            try:
-                reg[i[1]] = int(i[2])
-            except ValueError:
-                reg[i[1]] = reg[i[2]]
-            pc += 1
-        elif i[0] == 'mul':
-            try:
-                reg[i[1]] *= int(i[2])
-            except ValueError:
-                reg[i[1]] *= reg[i[2]]
-            pc += 1
-        elif i[0] == 'jgz':
-            pc += int(i[2]) if reg[i[1]] > 0 else 1
-        elif i[0] == 'snd':
-            try:
-                sounds.append(int(i[1]))
-            except ValueError:
-                sounds.append(reg[i[1]])
-            pc += 1
-        elif i[0] == 'add':
-            reg[i[1]] += int(i[2])
-            pc += 1
-        elif i[0] == 'mod':
-            try:
-                reg[i[1]] %= int(i[2])
-            except ValueError:
-                reg[i[1]] %= reg[i[2]]
-            pc += 1
-        elif i[0] == 'rcv':
-            print('recover {}'.format(sounds[-1]) if reg[i[1]] != 0 else 'no recovery')
-            if reg[i[1]] != 0:
-                break
-            pc += 1
+    def __init__(self, insns):
+        self.insns = [_ for _ in insns]
+        self.reg = {chr(ord('a') + i): 0 for i in range(26)}
+        self.pc = 0
+        self.sounds = []
+
+    def execute(self):
+        while self.pc >= 0 and self.pc < len(self.insns):
+            i = self.insns[self.pc]
+            print(self.pc, i)
+            if i[0] == 'set':
+                try:
+                    self.reg[i[1]] = int(i[2])
+                except ValueError:
+                    self.reg[i[1]] = self.reg[i[2]]
+                self.pc += 1
+            elif i[0] == 'mul':
+                try:
+                    self.reg[i[1]] *= int(i[2])
+                except ValueError:
+                    self.reg[i[1]] *= self.reg[i[2]]
+                self.pc += 1
+            elif i[0] == 'jgz':
+                self.pc += int(i[2]) if self.reg[i[1]] > 0 else 1
+            elif i[0] == 'snd':
+                try:
+                    self.sounds.append(int(i[1]))
+                except ValueError:
+                    self.sounds.append(self.reg[i[1]])
+                self.pc += 1
+            elif i[0] == 'add':
+                self.reg[i[1]] += int(i[2])
+                self.pc += 1
+            elif i[0] == 'mod':
+                try:
+                    self.reg[i[1]] %= int(i[2])
+                except ValueError:
+                    self.reg[i[1]] %= self.reg[i[2]]
+                self.pc += 1
+            elif i[0] == 'rcv':
+                print('recover {}'.format(self.sounds[-1]) if self.reg[i[1]] != 0 else 'no recovery')
+                if self.reg[i[1]] != 0:
+                    break
+                self.pc += 1
+
+with open('test.input' if len(sys.argv) != 2 else sys.argv[1], 'r') as fin:
+    insns = [_.split() for _ in fin]
+
+    a = Program(insns)
+    a.execute()
