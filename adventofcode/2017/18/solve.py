c1b152c3792c1ef10558d10fc163b0156ae3c9d3
Jonathan Wesley Stone <js@piratejon.com>
2017-12-22 13:39:37 -0600

    i am terrible at this

--- adventofcode/2017/18/solve.py
+++ adventofcode/2017/18/solve.py
@@ -5,20 +5,24 @@ import sys
 class Program:
     '''encapsulate the program so we can run two at once'''
 
-    def __init__(self, insns, pid):
+    def __repr__(self):
+        return 'Program {}: {}, {}:{}'.format(self.pid, self.status, self.pc, self.insns[self.pc])
+
+    def __init__(self, insns, pid=0):
+        self.pid = pid
         self.insns = [_ for _ in insns]
         self.reg = {chr(ord('a') + i): 0 for i in range(26)}
         self.pc = 0
         self.sounds = []
         self.pid = pid
         self.reg['p'] = pid
+        self.status = 'running'
 
     def tick(self, arb):
         if self.pc < 0 or self.pc >= len(self.insns):
-            raise IndexError('PC {} out of bounds [0, {})'.format(self.pc, len(self.insns)))
+            self.running = False
 
         i = self.insns[self.pc]
-        print(self.pc, i)
         if i[0] == 'set':
             try:
                 self.reg[i[1]] = int(i[2])
@@ -32,15 +36,23 @@ class Program:
                 self.reg[i[1]] *= self.reg[i[2]]
             self.pc += 1
         elif i[0] == 'jgz':
-            self.pc += int(i[2]) if self.reg[i[1]] > 0 else 1
+            try:
+                self.pc += int(i[2]) if self.reg[i[1]] > 0 else 1
+            except ValueError:
+                self.pc += self.reg[i[2]] if self.reg[i[1]] > 0 else 1
+            except KeyError:
+                self.pc += int(i[2]) if int(i[1]) > 0 else 1
         elif i[0] == 'snd':
             try:
-                arb.send(int(i[1]))
+                arb.send(self, int(i[1]))
             except ValueError:
-                arb.send(self.reg[i[1]])
+                arb.send(self, self.reg[i[1]])
             self.pc += 1
         elif i[0] == 'add':
-            self.reg[i[1]] += int(i[2])
+            try:
+                self.reg[i[1]] += int(i[2])
+            except ValueError:
+                self.reg[i[1]] += self.reg[i[2]]
             self.pc += 1
         elif i[0] == 'mod':
             try:
@@ -49,38 +61,79 @@ class Program:
                 self.reg[i[1]] %= self.reg[i[2]]
             self.pc += 1
         elif i[0] == 'rcv':
-            self.reg[i[1]] = arb.recv(self.reg[i[1]])
+            self.reg[i[1]] = arb.recv(self, self.reg[i[1]])
             self.pc += 1
 
 class ArbPart1:
-    '''send messages back and forth between two programs'''
+    '''wrapper for part1 to use the same framework as part2'''
 
     def __init__(self, insns):
-        a = Program(insns, 0)
+        a = Program(insns)
         self.value = None
 
-        while True:
-            try:
-                a.tick(self)
-            except ValueError:
-                break
+        while a.running:
+            print(a, a.pc, a.insns[a.pc])
+            a.tick(self)
 
-        print(self.value)
+        print('part1', self.value)
 
-    def send(self, value):
+    def send(self, caller, value):
         self.value = value
 
-    def recv(self, value):
+    def recv(self, caller, value):
         if value != 0:
-            raise ValueError
+            caller.running = False
         else:
             return value
+
+class ArbPart2:
+    '''two programs duet, sending and receiving amongst each other'''
+
+    def __init__(self, insns):
+        self.a, self.b = Program(insns, 0), Program(insns, 1)
+        self.other = {
+            self.a: self.b,
+            self.b: self.a
+        }
+        self.queue = {
+            self.a: [],
+            self.b: []
+        }
+        self.waiting = None
+
+        print(set(self.other.keys()))
+
+        self.a.status = 'running' if self.a.insns[0][0] != 'recv' else 'blocked'
+        self.b.status = 'running' if self.b.insns[0][0] != 'recv' else 'blocked'
+
+        prog = self.a
+        while self.a.status == 'running' or self.b.status == 'running':
+            print(prog)
+            if prog.status == 'running':
+                if self.queue[self.other[prog]] or prog.insns[prog.pc][0] != 'rcv':
+                    prog.tick(self)
+                else:
+                    prog.status == 'blocked'
+
+            prog = self.other[prog]
+
+    def send(self, caller, value):
+        self.queue[caller].append(value)
+        print(caller, 'send', value, 'queue', self.queue[caller])
+
+    def recv(self, caller, value):
+        x, self.queue[self.other[caller]] = self.queue[self.other[caller]][0], self.queue[self.other[caller]][1:]
+        print(caller, 'recv', x, 'queue', self.queue[self.other[caller]])
+        return x
 
 with open('test.input' if len(sys.argv) != 2 else sys.argv[1], 'r') as fin:
     insns = [_.split() for _ in fin]
 
     # part1
-    ArbPart1(insns)
+    try:
+        ArbPart1(insns)
+    except:
+        pass
 
     # part2
-    #arb = Arbiter(insns)
+    arb = ArbPart2(insns)
