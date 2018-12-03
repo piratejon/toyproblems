e1ffb6d7de35e241408f0e6da5409ef2640ac470
Jonathan Wesley Stone <js@piratejon.com>
2018-12-03 00:08:49 -0600

    moar stars

--- adventofcode/2018/03/load_data.sh
+++ adventofcode/2018/03/load_data.sh
@@ -90,3 +90,65 @@ select count(*) part_1 from unique_overlapping_claims
 ;
 EOF
 
+psql <<EOF
+with claims as (
+  select
+    rowid
+    , a
+    , x0
+    , y0
+    , x1
+    , y1
+    , row_number() over (order by x0, x1, y0, y1) rn
+  from (
+    select
+      rowid
+      , a
+      , b[3]::int x0
+      , b[4]::int y0
+      , b[6]::int w
+      , b[7]::int h
+      , b[3]::int + b[6]::int - 1 x1
+      , b[4]::int + b[7]::int - 1 y1
+    from (
+      select
+        rowid
+        , a
+        , regexp_split_to_array(a, '[ ,:x]') b
+      from aoc2018.input03
+    ) x
+  ) x
+)
+, find_overlaps as (
+  select
+    a.a a
+    , a.x0 ax0
+    , a.y0 ay0
+    , a.x1 ax1
+    , a.y1 ay1
+    , b.a b
+    , b.x0 bx0
+    , b.y0 by0
+    , b.x1 bx1
+    , b.y1 by1
+    , case
+      when a.x1 >= b.x0
+        and a.x0 <= b.x1
+        and a.y1 >= b.y0
+        and a.y0 <= b.y1
+      then 1 else 0 end
+    overlap
+  from claims a
+  inner join claims b
+    on a.rowid <> b.rowid
+    -- and a.x0 <= b.x0
+    -- and a.y0 <= b.y0
+)
+select
+  a part_2
+from find_overlaps
+group by a
+having sum(overlap) = 0
+;
+EOF
+
