8420988ac33beaee24a29da5cc8a407aef79261a
Jonathan Wesley Stone <js@piratejon.com>
2019-12-08 10:05:52 -0600

    woo yea

--- adventofcode/2019/05/solve.erl
+++ adventofcode/2019/05/solve.erl
@@ -40,38 +40,70 @@ init_program(Input) ->
 binop(1) -> fun(A, B) -> A + B end;
 binop(2) -> fun(A, B) -> A * B end.
 
-execute_binop(P, Opcode) ->
-  [Opcode, Ai, Bi, Oi | _] = program_read_tail(P#state.program, P#state.ip),
-  Read = [Opcode, Ai, Bi, Oi],
-  A = program_read(P#state.program, Ai),
-  B = program_read(P#state.program, Bi),
-  io:fwrite("execute_binop Read: ~w~n", [[Read | [A, B]]]),
-  P#state{ip=P#state.ip + 4, program=program_write(P#state.program, Oi, (binop(Opcode))(A, B))}.
+unpack_opcode(Opcode) ->
+  Size = 2 + case lists:reverse(List = integer_to_list(Opcode)) of
+           [$1 | _] -> 3;
+           [$2 | _] -> 3;
+           [$3 | _] -> 1;
+           [$4 | _] -> 1;
+           [$9, $9 | _] -> 0
+         end,
+  %io:fwrite("unpack_opcode: ~w~n", [[Opcode, Size]]),
+  %lists:reverse(lists:nthtail(Size - 1, [0 || _ <- lists:seq(1, Size)] ++ [list_to_integer([X]) || X <- List])).
+  lists:sublist(lists:reverse([0 || _ <- lists:seq(1, Size)] ++ [list_to_integer([X]) || X <- List]), Size).
 
-execute_input(P, Opcode) ->
-  [Opcode, Loc | _] = program_read_tail(P#state.program, P#state.ip),
+resolve_reference(P, Ref, Mode) ->
+  R = case Mode of
+    0 -> program_read(P#state.program, Ref);
+    1 -> Ref
+  end,
+  %io:fwrite("resolve_reference: ~w~n", [[Ref, Mode, R]]),
+  R.
+
+resolve_references(P, Refs, Modes) ->
+  Resolved = [resolve_reference(P, Ref, Mode) || {Ref, Mode} <- lists:zip(Refs, Modes)],
+  io:fwrite("resolve_references: ~w~n", [[Refs, Modes, Resolved]]),
+  Resolved.
+
+execute_binop(P, Opcode) ->
+  [Ar, Br, Cr | _] = program_read_tail(P#state.program, P#state.ip + 1),
+  % don't resolve C reference because it's always a position
+  [A, B, _] = resolve_references(P, [Ar, Br, Cr], lists:nthtail(2, Opcode)),
+  io:fwrite("execute_binop: ~w~n", [[Opcode, A, B, Cr]]),
+  {continue, P#state{ip=P#state.ip + 4, program=program_write(P#state.program, Cr, (binop(lists:nth(1, Opcode)))(A, B))}}.
+
+execute_input(P, Unpacked) ->
+  io:fwrite("execute_input: ~w~n", [Unpacked]),
+  [Loc | _] = program_read_tail(P#state.program, P#state.ip + 1),
+  io:fwrite("execute_input: ~w~n", [[Unpacked, Loc]]),
   [ReadInput | RestInput] = P#state.input,
-  P#state{
+  {continue, P#state{
     ip=P#state.ip + 2
     , input=RestInput
     , program=program_write(P#state.program, Loc, ReadInput)
-  }.
+  }}.
 
 execute_output(P, Opcode) ->
-  [Opcode, Loc | _] = program_read_tail(P#state.program, P#state.ip),
-  P#state{
+  [Loc | _] = program_read_tail(P#state.program, P#state.ip + 1),
+  io:fwrite("execute_output: ~w~n", [[Opcode, Loc]]),
+  {continue, P#state{
     ip=P#state.ip + 2
     , output=[program_read(P#state.program, Loc)] ++ P#state.output
-  }.
+  }}.
+
+execute_halt(P, _) -> {halt, P}.
 
 execute(P) ->
-  case lists:reverse(integer_to_list(Op = program_read(P#state.program, P#state.ip))) of
-    [$1 | _] -> {continue, execute_binop(P, Op)};
-    [$2 | _] -> {continue, execute_binop(P, Op)};
-    [$3 | _] -> {continue, execute_input(P, Op)};
-    [$4 | _] -> {continue, execute_output(P, Op)};
-    [$9, $9 | _] -> {halt, P}
-  end.
+  io:fwrite("dispatching ~w~n", [[P#state.ip, lists:sublist(program_read_tail(P#state.program, P#state.ip), 4)]]),
+  Fun = case Opcode = unpack_opcode(program_read(P#state.program, P#state.ip)) of
+    [1 | _] -> fun execute_binop/2;
+    [2 | _] -> fun execute_binop/2;
+    [3 | _] -> fun execute_input/2;
+    [4 | _] -> fun execute_output/2;
+    [9, 9 | _] -> fun execute_halt/2
+  end,
+  io:fwrite("dispatching ~w~n", [Opcode]),
+  Fun(P, Opcode).
 
 execute_loop(Program) ->
   case {_, NewProgram} = execute(Program) of
@@ -80,7 +112,7 @@ execute_loop(Program) ->
   end.
 
 part1() ->
-  Input = fetch_input('02-input'),
+  Input = fetch_input('input'),
   Program = init_program(Input),
   FinalState = execute_loop(Program),
   io:fwrite("Output ~w~n", [FinalState#state.output]),
