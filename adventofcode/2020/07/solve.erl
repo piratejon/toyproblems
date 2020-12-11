420b62ff026f935569401ced49ef5a092e911cc6
Jonathan Wesley Stone <js@piratejon.com>
2020-12-11 09:03:07 -0600

    now i have 14 stars!

--- adventofcode/2020/07/solve.erl
+++ adventofcode/2020/07/solve.erl
@@ -24,9 +24,7 @@ parse_line(Line) ->
   {string:join([A, C], "-"), dict:from_list(parse_rest_of_line(Rest))}.
 
 fetch_input(Filename) ->
-  Input = read_input_lines(Filename),
-  Parsed = dict:from_list([parse_line(Line) || Line <- Input]),
-  Parsed.
+  dict:from_list([parse_line(Line) || Line <- read_input_lines(Filename)]).
 
 fetch_int(String) ->
   case string:to_integer(String) of
@@ -34,19 +32,42 @@ fetch_int(String) ->
     _ -> wtf
   end.
 
+a_contains_b(Graph, Haystack, Needle) ->
+  case dict:find(Needle, dict:fetch(Haystack, Graph)) of
+    {ok, _} -> true;
+    _ -> lists:any(fun (H2) -> a_contains_b(Graph, H2, Needle) end, dict:fetch_keys(dict:fetch(Haystack, Graph)))
+  end.
+
+solve_part1(Graph, Key) ->
+  lists:filter(fun (H) -> a_contains_b(Graph, H, Key) end, dict:fetch_keys(Graph)).
+
+solve_part2(Graph, Bag) ->
+  dict:fold(
+    fun (Subbag, Count, Acc) ->
+        Acc + Count + (Count * solve_part2(Graph, Subbag)) end,
+    0, dict:fetch(Bag, Graph)).
+
 test() ->
-  Input = fetch_input('test.txt'),
-  io:fwrite("Test Input: ~p~n", [dict:fetch_keys(Input)]),
+  TestInputA = fetch_input('test.txt'),
+  io:fwrite("Test Input: ~p~n", [dict:fetch_keys(TestInputA)]),
+  TestPart1 = solve_part1(TestInputA, "shiny-gold"),
+  io:fwrite("Test 1: ~p: ~p~n", [length(TestPart1), TestPart1]),
+  TestPart2a = solve_part2(TestInputA, "shiny-gold"),
+  io:fwrite("Test 2a: ~p~n", [TestPart2a]),
+
+  TestInputB = fetch_input('test2.txt'),
+  TestPart2b = solve_part2(TestInputB, "shiny-gold"),
+  io:fwrite("Test 2b: ~p~n", [TestPart2b]),
   [].
 
 part1() ->
-  %Input = fetch_input('input.txt'),
-  %Part1 = lists:sum([count_group(Group) || Group <- Input]),
-  %io:fwrite("Part 1: ~p~n", [Part1]),
+  Input = fetch_input('input.txt'),
+  Part1 = solve_part1(Input, "shiny-gold"),
+  io:fwrite("Part1: ~p~n", [length(Part1)]),
   [].
 
 part2() ->
-  %Input = fetch_input('input.txt'),
-  %Part2 = lists:sum([do_part2(Group) || Group <- Input]),
-  %io:fwrite("Part 2: ~p~n", [Part2]),
+  Input = fetch_input('input.txt'),
+  Part2 = solve_part2(Input, "shiny-gold"),
+  io:fwrite("Part 2: ~p~n", [Part2]),
   [].
